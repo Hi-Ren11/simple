70c1c56e3e3f29c987634019731f6fbb7096c8f4
Augustine Omenuwa <Elitetech7827@gmail.com>
2023-10-18 01:50:56 -0700

    header file

---
+++ gmama.h
@@ -0,0 +1,15 @@
+#ifndef GMAMA_H
+#define GMAMA_H
+
+extern char **environ;
+
+/* header files */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* function prototype */
+char *_getline();
+
+
+#endif
